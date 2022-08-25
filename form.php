a55a04d0e6db2ffaf06d9ed4a5001bdc92d4d01f
yURa-inu <inuinu.meron@gmail.com>
2022-08-25 17:53:15 +0900

    add form.php

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang=ja>
+  <head>
+    <title>a</title>
+    <meta charset="utf-8">
+    <!-- css link -->
+  </head>
+  <body>
+    <header>
+      <div class="title">
+        <h1>booker</h1>
+      </div>
+    </header>
+    <main>
+      <p>名前</p>
+      <?php 
+        echo "aaa";
+      ?>
+    </main>
+    <footer></footer>
+  </body>
+</html>
